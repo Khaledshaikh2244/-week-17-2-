963435eab9cfae31caf0b0608bf54b524e425d27
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-11-25 10:27:58 +0530

    updated logic for chat/join

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,48 @@
-import { WebSocketServer } from "ws";
 
 
-const wss = new WebSocketServer ({port : 8080});
 
+import { WebSocketServer, WebSocket } from "ws";
 
-let userCount = 0;
-let AllSocket = [];
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (socket) =>{
-    AllSocket.push(socket);
-    userCount = userCount + 1;
-    console.log("user count" + userCount);
+interface User {
+    socket: WebSocket;
+    room: string;
+}
 
-    socket.on("message",(message) =>{
-        console.log("message",message.toString());
-        socket.send("mesasge",(message : any)=> {
-           console.log("message",message.toString());
+let allSockets: User[] = [];
 
-           for (let index = 0; index < AllSocket.length; index++) {
-            const element = AllSocket[index];
+wss.on("connection", (socket) => {
 
-            element.send(message.toString()+ ": sent frrom server" );
-           }
-        })
+    socket.on("message", (message) => {
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            console.log("user joined room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
+
+        if (parsedMessage.type == "chat") {
+            console.log("user wants to chat");
+            // const currentUserRoom = allSockets.find((x) => x.socket == socket).room
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
+
     })
-    
-    
-});
+
+})
+
